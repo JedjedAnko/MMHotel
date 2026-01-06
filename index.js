986f14b62cbfde2131fc6ad1d0b07921d0ee134c
Jedjedanko <jadejabagatangco2702@gmail.com>
2026-01-06 15:19:43 +0800

    v1.1.0

--- index.js
+++ index.js
@@ -170,3 +170,11 @@ function changeCoffeeImage(element) {
 
 
 
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
